48eff14288328a414b814b374af723d24485c956
noahpratt <56092410+noahpratt@users.noreply.github.com>
2019-10-26 17:42:06 -0400

    result updates

--- public/results.js
+++ public/results.js
@@ -1,5 +1,9 @@
 let content = document.getElementById("container");
 let loadingGif = document.getElementById("loadingGif");
+let image = document.getElementById("result-icon");
+let grade = document.getElementById("grade");
+let title = document.getElementById("result-title");
+
 let results = false;
 
 document.addEventListener('keydown', keyDownHandler, false);
@@ -12,6 +16,9 @@ function setTimer(){
 function checkResults(){
   if (results) {
     console.log("it's true!");
+    image.src = "img/0.png";
+    grade.innerHTML = "82%";
+    title.innerHTML = "Looks great!";
     content.style.display = "initial";
     loadingGif.style.display = "none";
     clearInterval(myTimer);
