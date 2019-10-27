f9d0d0fa1675508c5eb042138627c4f1a7394919
Michael Leung <michaelhtleung2@gmail.com>
2019-10-26 22:22:49 -0400

    Add POST function for file transfer

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,8 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+exports.transferImage = functions.https.onRequest((req, res) => {
+ console.log("===== IMAGE =====");
+ res.send("Image received!");
+});
