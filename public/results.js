c311177259d19d674c89b334046b699f15f6fde8
Michael Leung <michaelhtleung2@gmail.com>
2019-10-27 07:51:29 -0400

    Bare bones stable

--- public/results.js
+++ public/results.js
@@ -1,6 +1,23 @@
-let content = document.getElementById("container");
+let content = document.getElementById("resultsContainer");
 let loadingGif = document.getElementById("loadingGif");
+let title = document.getElementById("result-title");
+let subtitle = document.getElementById("keyword");
+let keyContent = document.getElementById("result-content");
+
 let results = false;
+
+// to be updated with how many keywords found
+let keywordResults = 5;
+let searchResults;
+searchResults = {
+  "keywords": [
+    {"name" : "keyword1", "content":["hey hello", "This is some content"]},
+    {"name" : "keyword2", "content":["hey hello", "This is some content", "look at that content!"]},
+    {"name" : "keyword3", "content":[]},
+    {"name" : "keyword4", "content":["hey hello", "This is some content"]},
+    {"name" : "keyword5", "content":["hey hello"]},
+  ]
+};
 
 document.addEventListener('keydown', keyDownHandler, false);
 window.addEventListener('load', setTimer);
@@ -12,6 +29,16 @@ function setTimer(){
 function checkResults(){
   if (results) {
     console.log("it's true!");
+
+    title.innerHTML = "[company title]";
+    let i, j, x = "";
+    for (i in searchResults.keywords) {
+      x += "<h3>" + searchResults.keywords[i].name + "</h3>";
+      for (j in searchResults.keywords[i].content) {
+        x += searchResults.keywords[i].content[j] + "<br>";
+      }
+    }
+    document.getElementById("result-content").innerHTML = x;
     content.style.display = "initial";
     loadingGif.style.display = "none";
     clearInterval(myTimer);
