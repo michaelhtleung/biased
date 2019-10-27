f37d59afcb4ab72340bc15372e0566bb268db0ec
Michael Leung <michaelhtleung2@gmail.com>
2019-10-27 02:41:30 -0400

    Add cloud storage listener

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
+const vision = require('@google-cloud/vision');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const client = new vision.ImageAnnotatorClient();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
@@ -10,4 +10,14 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 exports.transferImage = functions.https.onRequest((req, res) => {
  console.log("===== IMAGE =====");
  res.send("Image received!");
+});
+
+exports.identifyLogo = functions.storage.object().onFinalize(async (object) => {
+ const bucketName = 'yhack2019-d0dff.appspot.com';
+ const fileName = '/photos/myPictureName';
+
+ const [result] = await client.logoDetection(`gs://${bucketName}/${fileName}`);
+ const logos = result.logoAnnotations;
+ console.log('Logos:');
+ logos.forEach(logo => console.log(logo));
 });
