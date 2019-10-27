3e01b84d22616730b2d3c6f2c81deebcc2dde90d
Michael Leung <michaelhtleung2@gmail.com>
2019-10-27 08:48:32 -0400

    Demo ready

--- public/results.js
+++ public/results.js
@@ -11,11 +11,11 @@ let keywordResults = 5;
 let searchResults;
 searchResults = {
   "keywords": [
-    {"name" : "keyword1", "content":["hey hello", "This is some content"]},
-    {"name" : "keyword2", "content":["hey hello", "This is some content", "look at that content!"]},
-    {"name" : "keyword3", "content":[]},
-    {"name" : "keyword4", "content":["hey hello", "This is some content"]},
-    {"name" : "keyword5", "content":["hey hello"]},
+    {"name" : "Environment", "content":["microsoft operated carbon neutral since 2012 and continues to reduce our emissions."]},
+    {"name" : "Climate Change", "content":["Microsoft has been funding renewable energy points in Ireland and the Netherlands to prevent climate change."]},
+    // {"name" : "Pollution", "content":["Microsoft relied on the use of dirty diesel generators to power the quincy facility."]},
+    {"name" : "Energy", "content":["A majority of its data centers are powered by renewable energy."]},
+    // {"name" : "Sustainability", "content":["Microsoft has procured renewable energy to power its data center. 60% of its data centers are powered by renewable energy."]},
   ]
 };
 
@@ -23,6 +23,9 @@ document.addEventListener('keydown', keyDownHandler, false);
 window.addEventListener('load', setTimer);
 
 function setTimer(){
+	setTimeout(function(){
+	  results=true;
+	  },8000);
   myTimer = setInterval(checkResults, 200);
 }
 
@@ -30,7 +33,7 @@ function checkResults(){
   if (results) {
     console.log("it's true!");
 
-    title.innerHTML = "[company title]";
+    title.innerHTML = "Microsoft";
     let i, j, x = "";
     for (i in searchResults.keywords) {
       x += "<h3>" + searchResults.keywords[i].name + "</h3>";
@@ -39,6 +42,7 @@ function checkResults(){
       }
     }
     document.getElementById("result-content").innerHTML = x;
+    document.getElementById("result-content").style.fontSize = "12px";
     content.style.display = "initial";
     loadingGif.style.display = "none";
     clearInterval(myTimer);
