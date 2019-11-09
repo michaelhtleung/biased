f845384a3a80307abf9e27d09ce397c373d0c365
Michael Leung <michaelhtleung2@gmail.com>
2019-11-09 11:31:54 -0500

    Fix loading screen timing and add retry error message

--- public/index.js
+++ public/index.js
@@ -27,8 +27,11 @@ let fileUpload = document.getElementById("imageUpload");
 // GCP Cloud Service(s)
 let storageRef = firebase.storage().ref('photos/myPictureName');
 
-// Event Listener(s)
+// Event Listener(s) and Event Handler(s)
 fileUpload.addEventListener('change', function(evt) {
+  // change pages
+  scanPage.style.display = 'none';
+  loadPage.style.display = 'initial';
   let firstFile = evt.target.files[0]; // upload the first file only
   let uploadTask = storageRef.put(firstFile).then( () => {
     //  make ajax call to cloud function here
@@ -40,20 +43,24 @@ fileUpload.addEventListener('change', function(evt) {
       console.log(event.target.response);
       let responseObj = JSON.parse(event.target.response);
 
+      // change pages
       loadPage.style.display = 'none';
       resultsPage.style.display = 'initial';
 
       // dynamically load in data to results.html
-      title.innerHTML = responseObj.company;
-      document.getElementById("result-content-0").innerHTML = responseObj.paragraphs[0];
-      document.getElementById("result-content-1").innerHTML = responseObj.paragraphs[1];
-      document.getElementById("result-content-2").innerHTML = responseObj.paragraphs[2];
+      if (responseObj.company) {
+        title.innerHTML = responseObj.company;
+        document.getElementById("result-content-0").innerHTML = responseObj.paragraphs[0];
+        document.getElementById("result-content-1").innerHTML = responseObj.paragraphs[1];
+        document.getElementById("result-content-2").innerHTML = responseObj.paragraphs[2];
+      } else {
+        title.innerHTML = "Could not identify Logo";
+        document.getElementById("result-error-CTA").innerHTML = "Try Again?";
+      }
     }, false);
     let url = 'https://us-central1-yhack2019-d0dff.cloudfunctions.net/getSummary';
     let filepath = '/photos/myPictureName';
     ajax.open("GET", url + "?filepath=" + filepath);
     ajax.send();
-    scanPage.style.display = 'none';
-    loadPage.style.display = 'initial';
   });
 });
